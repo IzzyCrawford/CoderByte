32259e4321b030ae0654400b1cfdea89c4712717
Israel J Crawford <israel.crawford2011@gmail.com>
2020-08-21 01:29:41 -0500

    Update for Javascript and Reports

--- Global.asax.cs
+++ Global.asax.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using System.Web.Routing;
+using System.Web.Optimization;
+using CodeRepository.Web;
 
 namespace CoderByte
 {
@@ -13,6 +15,7 @@ namespace CoderByte
         {
             AreaRegistration.RegisterAllAreas();
             RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
         }
     }
 }
